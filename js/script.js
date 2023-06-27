3d3e2cd27c95d4a0234abaf81c258c4116ccc905
Flopiras <Flopiras@yahoo.it>
2023-06-27 14:52:37 +0200

    create 10 different mails

--- js/script.js
+++ js/script.js
@@ -5,9 +5,20 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const app = Vue.createApp({
     data() {
         return{
+            mails: []
 
         }
+    },
+    mounted() {
+        for(let i = 0; i < 10; i++){
+            axios.get(endpoint)
+            .then(res => {
+                console.log(res.data.response)
+                
+            })
+        }
+            
     }
 });
 
-app.mount('');
+app.mount('#root');
