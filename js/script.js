09482c4829207730f5446b4e21ab0736a67d5ef8
Flopiras <Flopiras@yahoo.it>
2023-06-28 09:19:48 +0200

    ask 10 DIFFERENT mails

--- js/script.js
+++ js/script.js
@@ -9,15 +9,31 @@ const app = Vue.createApp({
 
         }
     },
-    mounted() {
-        for(let i = 0; i < 10; i++){
+    methods: {
+        getRandomMails() {
             axios.get(endpoint)
             .then(res => {
-                this.mails.push(res.data.response);
+                const mail = res.data.response;
+                if(!this.mails.includes(mail)) this.mails.push(mail);
+                    if(this.mails.length < 10) this.getRandomMails();
             })
-        }
+        } 
+    },
+    mounted() {
+        this.getRandomMails();
+
+        /*while(this.mails.length < 10){
+            axios.get(endpoint)
+            .then(res => {
+                const mail = res.data.response;
+                if(!this.mails.includes(mail)) this.mails.push(mail);
+                   
+            })
+        } */
             
     }
 });
 
 app.mount('#root');
+
+//io chiedo una mail al programma. se la mail non è presente nell'array, la pusho. se l'array non ha 10 elementi, chiedo una mail al programma.
